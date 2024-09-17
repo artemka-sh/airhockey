5506de1575ead62051e5f801ce67796e6dd7aba5
artemka-sh <artemmmchik@mail.ru>
2024-09-17 20:07:47 +0300

    asdf

--- scripts/main.cs
+++ scripts/main.cs
@@ -14,23 +14,39 @@ public partial class main : Node2D
 		redScore = blueScore = 0;
 
 		var puck = GetNode<RigidBody2D>("puck");
+		var leftStick = GetNode<RigidBody2D>("blue");
+		var rightStick = GetNode<RigidBody2D>("red");
+
 		var puckStartPosition = GetNode<Marker2D>("PuckStartPosition");
-		puck.Position = puckStartPosition.Position;
+		var leftStickStartPosition = GetNode<Marker2D>("LeftStickStartPosition");
+		var rightStickStartPosition = GetNode<Marker2D>("RightStickStartPosition");
+
+		
+        // Перемещаем объекты на стартовые позиции
+        puck.Position = puckStartPosition.Position;
+        leftStick.Position = leftStickStartPosition.Position;
+        rightStick.Position = rightStickStartPosition.Position;
+
 
 	}
-
-
-
 
 	public override void _Ready()
 	{
 		NewGame();
 	}
 
-
-
-
 	public override void _Process(double delta)
 	{
+
+	}
+
+	public void onLeftGatesEntered(Node2D body)
+	{
+		NewGame();
+	}
+
+	public void onRightGatesEntered(Node2D body)
+	{
+		NewGame();
 	}
 }
